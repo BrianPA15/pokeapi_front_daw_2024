565118f97bc6c8fdcae39c1dea3d470400168d5f
BrianPA15 <brian.perugachi.angulo@alumnojoyfe.iepgroup.es>
2025-12-18 18:21:34 +0100

    prueba pa el profe

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-
+  <header>
+    <div class="title-container">
+      <img src="/img/title.png" alt="Pokédex" />
+    </div>
+  </header>
+  <RouterView />
 </template>
 
 <script setup>
